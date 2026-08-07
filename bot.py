acb9e36cb6a120239a3635f303b13774fa50329a
fadb4429-tech <fadb4429@gmail.com>
2026-08-07 10:50:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,6 +69,25 @@ JEUX = {
 
     },
 
+    "efootball": {
+
+        "nom": "Efootball 26",
+
+        "image": BASE_IMAGE + "efootball.jpg",
+
+        "url": "https://app.notion.com/p/PES-2017-2026-installation-33d2a32ee60680469bc2c2d8cc9b1a04"
+
+        "description"
+        "⚽️ Efootball26\n\n"
+
+        "Processor (CPU): Intel Core i5-2300 / AMD FX-4350"
+        
+        "Memory (RAM): 8 GB RAM"
+        "Storage: 70 GB available space"
+
+    },
+
+    
     "dmc": {
 
         "nom": "Devil May Cry 5",
